707c2d3855eadfd2a65e3d2a996220df9678414d
xxBei <zzw_bei@163.com>
2017-08-15 16:48:07 +0800

    ajax例子

---
+++ get.php
@@ -0,0 +1,12 @@
+<?php
+	// $a = $_GET['a'];
+	// $b = $_GET['b'];
+	$rdm = $_GET['rdm'];
+
+	// echo $a + $b;
+	echo "这是GET方法:";
+	echo "<br><br>";
+	echo '这是服务器端的随机数：'.$rdm;
+	
+
+?>
---
+++ post.php
@@ -0,0 +1,9 @@
+<?php 
+	// $a = $_POST['a'];
+	// $b = $_POST['b'];
+
+
+	echo "这是post方法";
+	echo "<br>";
+
+?>
